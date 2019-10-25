2b42b59e02942d1f206c274e0b611338c3b449e8
Monsur Khan <monsur.khan@gmail.com>
2019-10-25 00:30:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,6 +36,9 @@ end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
+  
+  matrix[row][column]
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
